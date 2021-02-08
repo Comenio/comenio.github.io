fc475fb4a03b80e7897ccdfc376209d20b8864e6
Vladimir Zahradnik <vladimir@zahradnik.io>
2021-02-08 11:31:50 +0100

    Add a landing page

---
+++ config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  siteTitle: 'Comenio Landing Page',
+  manifestName: 'Comenio',
+  manifestShortName: 'Comenio',
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/`,
+  // social
+  heading: 'Comenio',
+  subHeading:
+    "We're a e-learning platform, which gives instructors an option to host their courses on their own domain.",
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/Comenio',
+    },
+    {
+      icon: 'fa-envelope-o',
+      name: 'Email',
+      url: 'mailto:vladimir@zahradnik.io',
+    },
+  ],
+};
