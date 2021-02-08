4a770500326355dd8edfcef2ea7d52bb8b912386
Vladimir Zahradnik <vladimir@zahradnik.io>
2021-02-08 11:37:01 +0100

    Fix typo

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ module.exports = {
   // social
   heading: 'Comenio',
   subHeading:
-    "We're a e-learning platform, which gives instructors an option to host their courses on their own domain.",
+    "We're an e-learning platform, which gives instructors an option to host their courses on their own domain.",
   socialLinks: [
     {
       icon: 'fa-github',
