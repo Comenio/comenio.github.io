aadba43e83cfd5e30a3ba466c3cd1103ec3a5fa2
Vladimir Zahradnik <vladimir@zahradnik.io>
2021-04-11 17:45:07 +0200

    Add GDPR opt-in checkbox

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ module.exports = {
   // social
   heading: 'Comenio',
   subHeading:
-    "We're an e-learning platform, which gives instructors an option to host their courses on their own domain.",
+    "We're an e-learning platform, which gives instructors full control over their online courses.",
   socialLinks: [
     {
       icon: 'fa-github',
--- src/components/EmailForm.js
+++ src/components/EmailForm.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+const widgetUrl = 'https://app.mailjet.com/widget/iframe/4ezi/ItX';
+
 export class EmailForm extends Component {
   constructor() {
     super();
@@ -25,11 +27,14 @@ export class EmailForm extends Component {
           name="email"
           id="email"
           placeholder="Email Address"
+          required="required"
         />
         <input type="submit" value="Sign Up" />
         <span className={`${message ? 'visible success' : ''} message`}>
           {message}
         </span>
+        <input id="w-preview-consent-checkbox" type="checkbox" name="w-preview-consent-checkbox" required="required" />
+        <label style={{position: 'fixed', margin: '68px 0 0 0'}} htmlFor="w-preview-consent-checkbox">I agree to receive this newsletter and know that I can easily unsubscribe at any time.</label>
       </form>
     );
   }
